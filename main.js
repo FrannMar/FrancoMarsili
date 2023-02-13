6a9aea722a4e5be8e37e65cdd3ff5db84c16cb8b
Franco Marsili <92090620+FrannMar@users.noreply.github.com>
2023-02-14 00:54:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+alert ("Hola Usuario de Internet");
+let datosPersonales = {nombreC: "Franco Marsili", edad: 22, linkedin: "https://www.linkedin.com/in/franco-marsili-4a1565217/", 
+gitHub: "https://github.com/FrannMar" };
+console.log(datosPersonales);
